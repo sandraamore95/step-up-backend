d8e7d2858815d9a4514e4c6e0b1e823e1bd88e96
sandra <nasdrahlovejinx@hotmail.com>
2024-05-13 19:48:04 +0200

    modified endpoint addFavoriteShoe to user

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,27 +2,21 @@ const User = require("../models/User");
 
 
 const guardarEnListaDeseos = async (userData, shoeId) => {
-    try {
-        // Buscar al usuario por su ID y asegurarse de que exista
-        const usuario = await User.findById(userData._id);
-        if (!usuario) {
-            return { message: "Usuario no encontrado." };
-        }
+    // Buscar al usuario por su ID y asegurarse de que exista
+    const usuario = await User.findById(userData.id);
+    console.log(usuario);
+    if (!usuario) {
+        return { success: false, message: "Usuario no encontrado." };
+    }
 
-        // Verificar si la zapatilla ya está en la lista de deseos del usuario
-        const shoeIndex = usuario.favoriteShoes.indexOf(shoeId);
-        if (shoeIndex === -1) {
-            usuario.favoriteShoes.push(shoeId);
-            await usuario.save();
-
-            return { message: "Zapatilla guardada en la lista de deseos." };
-        } else {
-            // Si la zapatilla ya está en la lista de deseos -> error
-            return { message: "La zapatilla ya está en la lista de deseos." };
-        }
-    } catch (error) {
-        console.error("Error al guardar en lista de deseos:", error);
-        return { message: "Error al guardar en lista de deseos." };
+    // Verificar si la zapatilla ya está en la lista de deseos del usuario
+    const shoeIndex = usuario.favoriteShoes.indexOf(shoeId);
+    if (shoeIndex === -1) {
+        usuario.favoriteShoes.push(shoeId);
+        await usuario.save();
+        return { success: true, message: "Zapatilla guardada en la lista de deseos.", usuario };
+    } else {
+        return { success: false, message: "La zapatilla ya está en la lista de deseos." };
     }
 };
 
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -9,11 +9,25 @@ router.use(cors);
 
 
 //ruta protegida solo accesible por usuario autenticado
-router.post('/wishList/:zapatillaId', requireAuth, (req, res) => {
-    const userData = req.user;
-    const zapatillaId = req.params.zapatillaId; 
-    const result = accountController.guardarEnListaDeseos(userData, zapatillaId);
-    res.json(result);
+router.post('/wishList/:zapatillaId', requireAuth, async (req, res) => {
+    try {
+        const userData = req.user;
+        const zapatillaId = req.params.zapatillaId;
+        console.log(zapatillaId);
+       
+        const resultado = await accountController.guardarEnListaDeseos(userData, zapatillaId);
+        
+        if (resultado.success) {
+            //si es correcto
+            res.json({ success: true, message: resultado.message, usuario: resultado.usuario });
+        } else {
+            // Si ha fallado
+            res.status(400).json({ success: false, message: resultado.message });
+        }
+    } catch (error) {
+        console.error("Error en ruta /wishList:", error);
+        res.status(500).json({ success: false, message: "Error en ruta /wishList. Inténtalo de nuevo más tarde." });
+    }
 });
 
 module.exports = router;
